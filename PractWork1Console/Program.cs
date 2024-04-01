36b2da1a2406eca9822479a882adac76bab601d3
Izzat Aliyev <izzat@izzats-mbp.speedport.ip>
2024-04-02 00:24:22 +0200

    added console version of practWork1

---
+++ PractWork1Console/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PractWork1Console;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            System.Console.WriteLine(
+                """
+            Welcome to console app for calculating 2c+d math expression;
+            Author: Aliyev Izzat, ASTSI-20-1
+
+            """);
+
+            System.Console.Write("Enter value for c: ");
+            var valueC = float.TryParse(System.Console.ReadLine(), out var valueOfC) ? valueOfC : throw new Exception("Variable c is not a number !!!");
+            System.Console.Write("Enter value for d: ");
+            var valueD = float.TryParse(System.Console.ReadLine(), out var valueOfD) ? valueOfD : throw new Exception("Variable d is not a number !!!");
+
+            System.Console.WriteLine(
+                """
+                (1) Execute;
+                (0) Exit;
+                """);
+            var option = int.TryParse(System.Console.ReadLine(), out var optionValue) ? optionValue : throw new Exception("must be numeric!!!");
+            switch(option){
+                case 1: CalculateExpression(valueC, valueD); break;
+                case 0: CloseApp(); break;
+                default: System.Console.WriteLine("Option must be 0 or 1"); break;
+            }
+            
+            var sum = 2 * valueC + valueD;
+        }
+        catch(Exception ex)
+        {
+            System.Console.WriteLine(ex.Message);
+        }
+    }
+
+    private static void CalculateExpression(float valueC, float valueD)
+    {
+        var sum = 2 * valueC + valueD;
+        System.Console.WriteLine($"The result is {sum}");
+        CloseApp();
+    }
+
+    private static void CloseApp()
+    {
+        System.Environment.Exit(0);
+    }
+}
